47170cb14c23afac1f37e8efb56b74d4cd01bb16
prashantsingh <prashantsingh@fico.com>
2023-06-05 01:59:33 +0530

    "FrontEnd Design Completed"

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const path = require("path");
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get("/",(req, res, next)=>{
+    res.sendFile(path.join(__dirname,"views","index.html"));
+});
+
+
+
+
+
+
+
+app.listen(port, ()=>{
+    console.log(`server got started on port:  ${port}`);
+});
