b67638a6738cf2b74a2bf1c3023fae1e705f551c
prashantsingh <prashantsingh@fico.com>
2023-06-06 00:20:10 +0530

    Login page Implemented

--- index.js
+++ index.js
@@ -2,18 +2,39 @@ const express = require("express");
 
 const path = require("path");
 
+const bodyParser = require('body-parser');
+
 const app = express();
 
 const port = 3000;
 
 app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname,"data")));
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.get("/",(req, res, next)=>{
     res.sendFile(path.join(__dirname,"views","index.html"));
 });
 
-app.get("/")
+app.get("/downloadCV", (req, res, next)=>{
+    var filePath = path.join(__dirname,"/data/","PrashantSingh.pdf");
+    res.download(filePath);
+});
 
+app.post("/contact",(req, res, next)=>{
+    console.log(req.body);
+    res.status(302).redirect("/#contact");
+});
+
+app.get("/login", (req, res, next)=>{
+    res.sendFile(path.join(__dirname,"views","login.html"));
+});
+
+
+app.post("/login", (req, res, next)=>{
+    console.log(req.body);
+    res.status(200).redirect("/");
+});
 
 
 
